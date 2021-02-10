ff831b2d84736a5cc1a59de0cbbacddad228c7a1
Jack Chen <redchenjs@live.com>
2021-02-10 13:32:19 +0800

    app: update request code

--- app.js
+++ app.js
@@ -30,10 +30,13 @@ App({
   globalData: {
     reqCode: {
       HTTP_REQ_CODE_APP_GET_INFO    : 110,  // 微信端获取用户信息
-      HTTP_REQ_CODE_APP_GET_TOKEN   : 111,  // 微信端获取验证口令
-      HTTP_REQ_CODE_APP_BIND_USER   : 112,  // 微信端请求绑定用户
-      HTTP_REQ_CODE_APP_UNBIND_USER : 113,  // 微信端请求解绑用户
-      HTTP_REQ_CODE_APP_UPDATE_PSWD : 114   // 微信端请求修改密码
+      HTTP_REQ_CODE_APP_SET_TIME    : 111,  // 微信端设定预约时间
+      HTTP_REQ_CODE_APP_SET_CANCEL  : 112,  // 微信端取消预约时间
+      HTTP_REQ_CODE_APP_SET_ONLINE  : 113,  // 微信端请求允许上机
+      HTTP_REQ_CODE_APP_SET_OFFLINE : 114,  // 微信端请求强制下机
+      HTTP_REQ_CODE_APP_BIND_USER   : 115,  // 微信端请求绑定用户
+      HTTP_REQ_CODE_APP_UNBIND_USER : 116,  // 微信端请求解绑用户
+      HTTP_REQ_CODE_APP_UPDATE_PSWD : 117   // 微信端请求修改密码
     },
     userCode: null,
     serverUrl: 'https://server.zyiot.top/lem/'
